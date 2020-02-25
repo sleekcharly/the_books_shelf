9076036df070b0b9235d9e71a7f43485748e429c
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-25 11:44:24 +0100

    implemented password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         minlength: 6
     },
     name: {
@@ -34,6 +34,24 @@ const userSchema = mongoose.Schema({
         type: String
     }
 });
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT, function(err, salt){
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
